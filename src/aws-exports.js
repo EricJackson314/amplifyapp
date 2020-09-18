d4473b7134272dc3842faa8e927899d422924673
Eric Jackson <ebj29@cornell.edu>
2020-09-18 10:19:16 -0400

    notes

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -7,7 +7,11 @@ const awsmobile = {
     "aws_cognito_region": "us-west-2",
     "aws_user_pools_id": "us-west-2_hl0mWEfbC",
     "aws_user_pools_web_client_id": "4ashdv3vl3085ns6s8pu83uopm",
-    "oauth": {}
+    "oauth": {},
+    "aws_appsync_graphqlEndpoint": "https://rmrmrhyczbclnftvz4tupavx5q.appsync-api.us-west-2.amazonaws.com/graphql",
+    "aws_appsync_region": "us-west-2",
+    "aws_appsync_authenticationType": "API_KEY",
+    "aws_appsync_apiKey": "da2-rvzcief37bbrrbk2eplghbez3q"
 };
 
 
